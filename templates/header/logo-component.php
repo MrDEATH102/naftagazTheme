182a086fe52c54ab1f94808911cf64e9e8d07e1b
MrDEATH102 <massamongus@gmail.com>
2025-06-08 09:51:13 +0330

    installable file

---
+++ templates/header/logo-component.php
@@ -0,0 +1,5 @@
+<div class="logo__header">
+    <a href="<?php bloginfo('url'); ?>">
+        <img src="" alt="<?php bloginfo('name');?>" title="<?php bloginfo('name');?>" class="img-fluid">
+    </a>
+</div>
