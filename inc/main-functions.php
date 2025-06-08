8c617fb0b1646d2dd03b36f6918b61429cb3d423
MrDEATH102 <massamongus@gmail.com>
2025-06-08 13:49:55 +0330

    header

--- inc/main-functions.php
+++ inc/main-functions.php
@@ -4,9 +4,26 @@ function naftagaz_style_script()
 {
 
     // enqueue styles 
-    wp_enqueue_style('main-style', get_template_directory_uri() . 'assets/css/main-style.css', array(), '1.0.0', 'all');
+    wp_enqueue_style( 'index', get_template_directory_uri().'/assets/css/index.css' , array(), '1.0.0', 'all' );
 
     // enqueue scripts 
-    wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script( 'main', get_template_directory_uri().'/assets/js/main.js', array('jquery'), '1.0.0', true );
 }
 add_action('wp_enqueue_scripts', 'naftagaz_style_script');
+
+
+// Register nav menus 
+if (! function_exists('naftagaz_nav_menus')) {
+
+    function naftagaz_nav_menus()
+    {
+        register_nav_menus(
+            array(
+                'header_menu'   => 'Header Menu',
+                'footer_menu'   => 'Footer Menu'
+            )
+        );
+    }
+
+    add_action('after_setup_theme', 'naftagaz_nav_menus');
+}
---
+++ templates/header/menu-component.php
@@ -0,0 +1,9 @@
+<div class="menu__header">
+    <?php
+        wp_nav_menu(
+            array(
+                'theme_location'    => 'header_menu',
+            )
+        );
+    ?>
+</div>
