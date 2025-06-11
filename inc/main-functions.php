e817d8ac8d83b53d0e0bf5663cc470ec159a60f7
MrDEATH102 <massamongus@gmail.com>
2025-06-11 12:27:46 +0330

    enqueue styles

--- inc/main-functions.php
+++ inc/main-functions.php
@@ -4,14 +4,17 @@ function naftagaz_style_script()
 {
 
     // enqueue styles 
-    wp_enqueue_style( 'index', get_template_directory_uri().'/assets/css/index.css' , array(), '1.0.0', 'all' );
-    wp_enqueue_style('company', get_template_directory_uri().'/assets/css/company.css', array(), '1.0.0', 'all');
-    wp_enqueue_style('services_drilling', get_template_directory_uri().'assets/css/services_drilling.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('index', get_template_directory_uri() . '/assets/css/index.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('company', get_template_directory_uri() . '/assets/css/company.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('services_drilling', get_template_directory_uri() . 'assets/css/services_drilling.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('services_service', get_template_directory_uri() . 'assets/css/services_service.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('careers-succes-stories', get_template_directory_uri() . 'assets/css/careers-succes-stories.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('careers-recognition-board', get_template_directory_uri() . 'assets/css/careers-recognition-board.css', array(), '1.0.0', 'all');
 
     // enqueue scripts 
-    wp_enqueue_script( 'index', get_template_directory_uri().'/assets/js/index.js', array('jquery'), '1.0.0', true );
-    wp_enqueue_script('company', get_template_directory_uri().'/assets/js/company.js', array('jquery'), '1.0.0', true);
-    wp_enqueue_script('services_drilling', get_template_directory_uri().'/assets/js/services_drilling.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('index', get_template_directory_uri() . '/assets/js/index.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('company', get_template_directory_uri() . '/assets/js/company.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('services_drilling', get_template_directory_uri() . '/assets/js/services_drilling.js', array('jquery'), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'naftagaz_style_script');
 
